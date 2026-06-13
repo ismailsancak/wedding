6c8cf835ec8bcdbaef483f9cc8408f03cfd9e45f
ismailsancak <ismailsancak13@gmail.com>
2026-06-13 21:30:01 +0300

    Delete api directory

--- api/uploadthing/route.ts
+++
@@ -1,9 +0,0 @@
-import { createRouteHandler } from "uploadthing/next";
-
-import { ourFileRouter } from "./core";
-
-// Export routes for Next App Routers
-export const { GET, POST } = createRouteHandler({
-  router: ourFileRouter,
-  // config: {},
-});
